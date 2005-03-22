f24c2cf8ca6bf40aeabdb0bedd594774234020f3
glehmann <none>
2005-03-22 10:26:22 +0100

    trunk: changeset 5
    correction des scripts de synchro mdk
    
    darcs-hash:20050322092622-9c3ed-118e56e45f39c31b5da614efbfd79907c73c6bac.gz

--- trunk/install_current_media.sh
+++ trunk/install_current_media.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
-case "$1" in
-    -h|--hdlist)
-	LIST=hdlist.cz
-	;;
-    *)
-	LIST=synthesis.hdlist.cz
-esac
-
 VERSION=current
 
-urpmi.addmedia -q --update main http://voxel.jouy.inra.fr/mdk/$VERSION/i586/media/main/ with media_info/$LIST
-urpmi.addmedia -q --update contrib http://voxel.jouy.inra.fr/mdk/$VERSION/i586/media/contrib with media_info/$LIST
-urpmi.addmedia -q --update jpackage http://voxel.jouy.inra.fr/mdk/$VERSION/i586/media/jpackage with media_info/$LIST
-urpmi.addmedia -q --update updates http://voxel.jouy.inra.fr/mdk/$VERSION/main_updates/ with media_info/$LIST
-urpmi.addmedia -q --update comm http://voxel.jouy.inra.fr/mdk/$VERSION/comm/ with media_info/$LIST
-urpmi.addmedia -q --update bdr http://voxel.jouy.inra.fr/mdk/$VERSION/bdr with media_info/$LIST
-urpmi.addmedia -q --update plf-free http://voxel.jouy.inra.fr/mdk/$VERSION/plf/free/ with $LIST
-urpmi.addmedia -q --update plf-non-free http://voxel.jouy.inra.fr/mdk/$VERSION/plf/non-free/ with $LIST
+LIST="synthesis.hdlist.cz"
+WGET=""
+
+while [ "$1" != "" ] ; do
+    case "$1" in
+        -H|--hdlist)
+	    LIST=hdlist.cz
+	    ;;
+	-w|--wget)
+	    WGET="--wget"
+	    ;;
+	-h|--help)
+	    echo "-H --hdlist   installe les media avec les hdlist au lieu des synthesis"
+	    echo "-w --wget     utilise wget pour recuperer les fichiers"
+	    exit 1
+    esac
+    shift
+done
+
+urpmi.addmedia -q --update $WGET main http://voxel.jouy.inra.fr/mdk/$VERSION/i586/media/main/ with media_info/$LIST
+urpmi.addmedia -q --update $WGET contrib http://voxel.jouy.inra.fr/mdk/$VERSION/i586/media/contrib with media_info/$LIST
+urpmi.addmedia -q --update $WGET jpackage http://voxel.jouy.inra.fr/mdk/$VERSION/i586/media/jpackage with media_info/$LIST
+urpmi.addmedia -q --update $WGET updates http://voxel.jouy.inra.fr/mdk/$VERSION/main_updates/ with media_info/$LIST
+urpmi.addmedia -q --update $WGET comm http://voxel.jouy.inra.fr/mdk/$VERSION/comm/ with media_info/$LIST
+urpmi.addmedia -q --update $WGET bdr http://voxel.jouy.inra.fr/mdk/$VERSION/bdr with media_info/$LIST
+urpmi.addmedia -q --update $WGET plf-free http://voxel.jouy.inra.fr/mdk/$VERSION/plf/free/ with $LIST
+urpmi.addmedia -q --update $WGET plf-non-free http://voxel.jouy.inra.fr/mdk/$VERSION/plf/non-free/ with $LIST
