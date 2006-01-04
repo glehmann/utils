9547870d7138a7d498107889e599223b0716d097
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-01-04 16:48:53 +0100

    use lvm snapshot for the backup
    darcs-hash:20060104154853-2fc9d-d29721b212917b1d75dbbf799556d21d7a4e7df2.gz

--- voxel2pixel.sh
+++ voxel2pixel.sh
@@ -4,16 +4,33 @@
 # 
 #
 
+
+# necessaire pour le snapshot lvm
+modprobe dm_snapshot
+
+# creation du snapshot avece 10G de tampon
+lvcreate --size 10G --snapshot --name snap /dev/data/home
+
+# creation d'un repertoire et montage du snapshot
+mkdir -p /root/home-snap
+mount /dev/data/snap /root/home-snap/
+
+# la sauvegarde
 rdiff-backup \
    -v 5 \
    --ssh-no-compression \
-   --exclude /home/darcs/ \
-   --exclude /home/kitware/ \
-   --exclude /home/lost+found/ \
-   --exclude /home/TT_DB/ \
-   /home pixel::/backup/voxel/archives \
+   --exclude /root/home-snap/darcs/ \
+   --exclude /root/home-snap/kitware/ \
+   --exclude /root/home-snap/lost+found/ \
+   --exclude /root/home-snap/TT_DB/ \
+   /root/home-snap/ pixel::/backup/voxel/archives \
 || echo 'il faut vite faire quelquechose !' | mail -s 'Alerte: rdiff-backup a echoue sur voxel' gaetan.lehmann@jouy.inra.fr christophe.caron@jouy.inra.fr
 
+# demontage du snapshot et suppression du repertoire
+umount /root/home-snap
+rmdir /root/home-snap
 
-
+# suppression du snapshot
+# --force est necessaire pour que lvremove ne demandre pas de confirmation
+lvremove --force /dev/data/snap
 
