2e5f50749386719865e8662c0ef4a05d8b424546
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-01-04 18:17:11 +0100

    improve error management
    darcs-hash:20060104171711-2fc9d-ce1ae65b685678278cb85fc75041487abe0a717f.gz

--- voxel2pixel.sh
+++ voxel2pixel.sh
@@ -5,32 +5,73 @@
 #
 
 
+DEST_DIR=pixel::/backup/voxel/archives
+SNAP_DIR=/root/home-snap
+
+removeSnapshot() {
+  lvremove --force /dev/data/snap || warn "Erreur lors de la suppression du snapshot"
+}
+
+removeMountPoint() {
+  umount $SNAP_DIR || warn "Erreur lors du demontage du snapshot"
+}
+
+
+# les procedures d'erreur
+
+warn() {
+  SUBJECT="Alerte: Echec sauvegarde sur voxel."
+  TO="gaetan.lehmann@jouy.inra.fr christophe.caron@jouy.inra.fr"
+  echo "$1" | mail -s "$SUBJECT $1" $TO
+}
+
+error() {
+  warn "$1"
+  exit 1
+}
+
+
+mountFailed() {
+  removeSnapshot
+  error "Erreur lors du montage du snapshot"
+}
+
+backupFailed() {
+  removeMountPoint
+  removeSnapshot 
+  rdiff-backup --check-destination-dir $DEST_DIR
+  error "rdiff-backup a echoue"
+}
+
+
+
+
 # necessaire pour le snapshot lvm
-modprobe dm_snapshot
+modprobe dm_snapshot || error "Impossible de charger le module dm_snapshot"
 
 # creation du snapshot avece 10G de tampon
-lvcreate --size 10G --snapshot --name snap /dev/data/home
+lvcreate --size 10G --snapshot --name snap /dev/data/home || error "Erreur lors de la creation du snapshot"
 
 # creation d'un repertoire et montage du snapshot
-mkdir -p /root/home-snap
-mount /dev/data/snap /root/home-snap/
+mkdir -p $SNAP_DIR
+mount /dev/data/snap $SNAP_DIR  || mountFailed
 
 # la sauvegarde
 rdiff-backup \
    -v 5 \
    --ssh-no-compression \
-   --exclude /root/home-snap/darcs/ \
-   --exclude /root/home-snap/kitware/ \
-   --exclude /root/home-snap/lost+found/ \
-   --exclude /root/home-snap/TT_DB/ \
-   /root/home-snap/ pixel::/backup/voxel/archives \
-|| echo 'il faut vite faire quelquechose !' | mail -s 'Alerte: rdiff-backup a echoue sur voxel' gaetan.lehmann@jouy.inra.fr christophe.caron@jouy.inra.fr
+   --exclude $SNAP_DIR/darcs/ \
+   --exclude $SNAP_DIR/kitware/ \
+   --exclude $SNAP_DIR/lost+found/ \
+   --exclude $SNAP_DIR/TT_DB/ \
+   $SNAP_DIR \
+   $DEST_DIR \
+|| backupFailed
 
 # demontage du snapshot et suppression du repertoire
-umount /root/home-snap
-rmdir /root/home-snap
+removeMountPoint 
 
 # suppression du snapshot
 # --force est necessaire pour que lvremove ne demandre pas de confirmation
-lvremove --force /dev/data/snap
+removeSnapshot 
 
