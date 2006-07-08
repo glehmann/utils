b746b127927306c75e9588e95de1738214ec4f64
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-07-08 12:38:50 +0200

    nightly created zip file
    darcs-hash:20060708103850-2fc9d-63f1eae5ab35ce6a4105e122938e547e54734a82.gz

--- tailor-itk.sh
+++ tailor-itk.sh
@@ -7,3 +7,7 @@ darcs tag -m `date +%F`
 
 cd /darcs/contrib-itk/WrapITK
 darcs dist
+cd /tmp
+rm -rf /tmp/WrapITK
+tar xzf /darcs/contrib-itk/WrapITK/WrapITK.tar.gz
+zip /darcs/contrib-itk/WrapITK/WrapITK.zip WrapITK
