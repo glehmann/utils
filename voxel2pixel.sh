a3fc76b50db88d2804fb6972afa505e15a6e5773
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2008-10-21 15:03:23 +0200

    adaptation de la procedure pour les nouveaux serveurs
    darcs-hash:20081021130323-2fc9d-e51daa8cac0ab40f3b05c0fffa83a790ddc980a7.gz

--- voxel2pixel.sh
+++ voxel2pixel.sh
@@ -4,25 +4,22 @@
 # 
 #
 
-
-DEST_DIR=pixel::/backup/voxel/archives
-SNAP_DIR=/root/home-snap
+DATE=`date +%F`
 
 removeSnapshot() {
-  lvremove --force /dev/data/snap || warn "Erreur lors de la suppression du snapshot"
+  ssh voxel lvremove --force /dev/data/snap || warn "Erreur lors de la suppression du snapshot"
 }
 
 removeMountPoint() {
-  umount $SNAP_DIR/papha/  || warn "Erreur lors du demontage du partage nfs"
-  umount $SNAP_DIR || warn "Erreur lors du demontage du snapshot"
+  ssh voxel umount /mnt/home-snap || warn "Erreur lors du demontage du snapshot"
 }
 
 
 # les procedures d'erreur
 
 warn() {
-  SUBJECT="Alerte: Echec sauvegarde sur voxel."
-  TO="gaetan.lehmann@jouy.inra.fr christophe.caron@jouy.inra.fr"
+  SUBJECT="Alerte: Echec sauvegarde voxel ($DATE)."
+  TO="gaetan.lehmann@jouy.inra.fr" # christophe.caron@jouy.inra.fr"
   echo "$1" | mail -s "$SUBJECT $1" $TO
 }
 
@@ -40,7 +37,7 @@ mountFailed() {
 backupFailed() {
   removeMountPoint
   removeSnapshot 
-  rdiff-backup --check-destination-dir $DEST_DIR
+  rdiff-backup --check-destination-dir /data/voxel-images/
   error "rdiff-backup a echoue"
 }
 
@@ -48,28 +45,28 @@ backupFailed() {
 
 
 # necessaire pour le snapshot lvm
-modprobe dm_snapshot || error "Impossible de charger le module dm_snapshot"
+ssh voxel modprobe dm_snapshot || error "Impossible de charger le module dm_snapshot"
 
 # creation du snapshot avece 10G de tampon
-lvcreate --size 10G --snapshot --name snap /dev/data/home || error "Erreur lors de la creation du snapshot"
+ssh voxel lvcreate --size 100G --snapshot --name snap /dev/data/home || error "Erreur lors de la creation du snapshot"
 
 # creation d'un repertoire et montage du snapshot
-mkdir -p $SNAP_DIR
-mount /dev/data/snap $SNAP_DIR  || mountFailed
-mount --bind /home/papha/ $SNAP_DIR/papha/ || mountFailed
+ssh voxel mkdir -p /mnt/home-snap
+ssh voxel chmod 700 /mnt/home-snap
+ssh voxel mount -o ro /dev/data/snap /mnt/home-snap  || mountFailed
 
 # la sauvegarde
 rdiff-backup \
    -v 5 \
+   --backup-mode \
    --ssh-no-compression \
-   --exclude $SNAP_DIR/darcs/ \
-   --exclude $SNAP_DIR/kitware/ \
-   --exclude $SNAP_DIR/lost+found/ \
-   --exclude $SNAP_DIR/TT_DB/ \
+   --exclude-device-files \
+   --exclude-fifos \
    --exclude-regexp '.*/non archive/.*' \
-   $SNAP_DIR \
-   $DEST_DIR \
+   voxel::/mnt/home-snap/home/ \
+   /data/voxel-images/ \
 || backupFailed
+#   --exclude-sockets \
 
 # demontage du snapshot et suppression du repertoire
 removeMountPoint 
@@ -78,3 +75,5 @@ removeMountPoint
 # --force est necessaire pour que lvremove ne demandre pas de confirmation
 removeSnapshot 
 
+# copie du repertoire de backup sur voxel pour l'avoir en double
+rsync -avH --delete /data/voxel-images/rdiff-backup-data voxel:/data/ || error "rsync a echoue."
