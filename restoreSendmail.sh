43b28ab31d1c9899a2b7759159c37998c66c54d2
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2010-11-15 10:05:53 +0100

    use zlogin instead of ssh
    Ignore-this: f9ec4735ab56cfc59b84eed8ebfc4d85
    
    darcs-hash:20101115090553-2fc9d-1907e34896aca1b04c477a01ff41aae10cc74ba0.gz

--- restoreSendmail.sh
+++ restoreSendmail.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+echo restoring sendmail on $HOSTNAME
 /usr/ccs/bin/m4 -D_CF_DIR_=/etc/mail/cf/ /etc/mail/cf/m4/cf.m4 /etc/mail/sendmail.m4 > /etc/mail/sendmail.cf
 svcadm restart sendmail-client smtp:sendmail
 echo test sendmail $HOSTNAME | mailx -s "$HOSTNAME" root
@@ -7,6 +8,6 @@ echo test sendmail $HOSTNAME | mailx -s "$HOSTNAME" root
 
 if [ -n "`zoneadm list | grep '^global$'`" ]; then
   for h in `zoneadm list | grep -v '^global$'`; do
-    ssh $h /usr/local/bin/restoreSendmail.sh
+    zlogin $h /usr/local/bin/restoreSendmail.sh
   done
 fi
